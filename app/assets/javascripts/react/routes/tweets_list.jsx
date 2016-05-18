2f0701563f0103445f50760c5e9308033466d287
schwannden <schwannden@gmail.com>
2016-05-18 16:38:16 +0800

    fix Tweet Display Seequence #5

--- app/assets/javascripts/react/routes/tweets_list.jsx
+++ app/assets/javascripts/react/routes/tweets_list.jsx
@@ -5,17 +5,19 @@ import Tweet    from 'routes/tweet'
 
 export default class TweetsList extends React.Component {
   render () {
-    let tweets_left = [], tweets_right = [];
+    let tweets_left = [], tweets_right = [], tweets = [];
     this.props.tweets.map( (tweet, i) => {
-      tweet.float_direction = (i%2 == 0)? 
+      tweets.push(<Tweet {...tweet} />);
+      (i%2 == 0)? 
         tweets_left.push(<Tweet {...tweet} />) : 
         tweets_right.push(<Tweet {...tweet} />);
     });
     return (
       <div>
         <TweetBox />
-        <Col sm={6}>{tweets_left}</Col> 
-        <Col sm={6}>{tweets_right}</Col>
+        <Col sm={6} className='hidden-md'>{tweets_left}</Col> 
+        <Col sm={6} className='hidden-md'>{tweets_right}</Col>
+        <Col sm={12} className='visible-md'>{tweets}</Col>
       </div>
     );
   }
